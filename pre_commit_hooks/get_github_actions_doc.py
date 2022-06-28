74e31b69f1744ad8508418c7559fea81abb18d5d
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 14:13:21 -0300

    reste

--- pre_commit_hooks/get_github_actions_doc.py
+++ pre_commit_hooks/get_github_actions_doc.py
@@ -16,19 +16,18 @@ def main(argv: Sequence[str] | None = None) -> int:
         try:
             with open(filename, encoding='UTF-8') as f:
                 actions = yaml.load(f)
-                readme=open('README.md', encoding='UTF-8').read()
-                
-                pattern = re.compile("<!--BEGIN DOC-->.*<!--END DOC-->", re.DOTALL | re.MULTILINE)
-                if pattern.match(readme):
-                    readme = pattern.sub("\n<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->\n", readme)            
-                else:
-                    readme = readme + "\n<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->\n"            
-                open("README.md", 'wb').write(readme.encode("utf-8"))
         except yaml.YAMLError as exc:
             print(exc)
             retval = 1
         except:
             retval = 1
+        readme=open('README.md', encoding='UTF-8').read()
+        pattern = re.compile("BEGIN DOC.*END DOC", re.DOTALL | re.MULTILINE)
+        if pattern.match(readme):
+            readme = pattern.sub("\nBEGIN DOC\nSecond Text\nEND DOC\n", readme)            
+        else:
+            readme = readme + "\nBEGIN DOCnFirst TextEND DOC\n"            
+        open("README.md", 'wb').write(readme.encode("utf-8"))
 
         return retval
 
