b39b0131377088a3bdb9ea0ac2e3d04b7f7e24b5
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 15:22:19 -0300

    Always run

--- pre_commit_hooks/get_github_actions_doc.py
+++ pre_commit_hooks/get_github_actions_doc.py
@@ -11,25 +11,24 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument('filenames', nargs='*', help='Filenames to check.')
     args = parser.parse_args(argv)
     retval = 0
-    for filename in args.filenames:
-        print("FILENAME: ", filename)
-        try:
-            with open(filename, encoding='UTF-8') as f:
-                actions = yaml.load(f)
+    filename = ".github/workflow/actions.yaml"
+    print("FILENAME: ", filename)
+    try:
+        with open(filename, encoding='UTF-8') as f:
+            actions = yaml.load(f)
+        doc_string = "A document string"
+        readme=open('README.md', encoding='UTF-8').read()
+        pattern = re.compile(r"(.*?)<!--BEGIN_DOC-->.*<!--END_DOC-->(.*)", flags=(re.DOTALL | re.MULTILINE))
+        if pattern.match(readme):
+            readme = pattern.sub("\\1<!--BEGIN_DOC-->\n{}\n<!--END_DOC-->\\2".format(doc_string), readme)
+        else:
+            print("Did not find doc")
+            readme = "{}\n<!--BEGIN_DOC-->\n{}\n<!--END_DOC-->\n".format(readme, doc_string)
+        open("README.md", 'wb').write(readme.encode("utf-8"))
 
-            doc_string = "A document string"
-            readme=open('README.md', encoding='UTF-8').read()
-            pattern = re.compile(r"(.*?)<!--BEGIN_DOC-->.*<!--END_DOC-->(.*)", flags=(re.DOTALL | re.MULTILINE))
-            if pattern.match(readme):
-                readme = pattern.sub("\\1<!--BEGIN_DOC-->\n{}\n<!--END_DOC-->\\2".format(doc_string), readme)
-            else:
-                print("Did not find doc")
-                readme = "{}\n<!--BEGIN_DOC-->\n{}\n<!--END_DOC-->\n".format(readme, doc_string)
-            open("README.md", 'wb').write(readme.encode("utf-8"))
-
-        except Exception as exc:
-            print("EXCEPTION HAS OCCURRED: ", exc)
-            retval = 1
+    except Exception as exc:
+        print("EXCEPTION HAS OCCURRED: ", exc)
+        retval = 1
 
     return retval
 
