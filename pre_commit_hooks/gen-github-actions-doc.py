0a8232488973b79b2bb5ae45cc0b355bbb9fd6a7
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 12:29:27 -0300

    Write to readme file

--- pre_commit_hooks/gen-github-actions-doc.py
+++ pre_commit_hooks/gen-github-actions-doc.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import re
 from typing import Sequence
 
 from pre_commit_hooks.check_yaml import yaml
@@ -15,10 +16,23 @@ def main(argv: Sequence[str] | None = None) -> int:
         try:
             with open(filename, encoding='UTF-8') as f:
                 actions = yaml.load(f)
-                print(actions)
+                readme=""
+                with open("README.md", 'r') as f:
+                    readme = f.read().decode('utf8')
+                pattern = re.compile(r"""<!--BEGIN DOC-->.*<!--END DOC>""",re.MULTILINE)
+                if pattern.match(readme):
+                    readme = pattern.sub("<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->", readme)            
+                else:
+                    readme = readme + "<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->"            
+                with open("README.md", 'w') as f:
+                    f.write(readme.encode("utf-8"))
+
         except yaml.YAMLError as exc:
             print(exc)
             retval = 1
+        except:
+            retval = 1
+
     return retval
 
 if __name__ == '__main__':
