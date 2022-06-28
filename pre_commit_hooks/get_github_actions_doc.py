54b4d9363f85ef98ba2747b3fb0e119335100f63
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 13:52:32 -0300

    teste

--- pre_commit_hooks/get_github_actions_doc.py
+++ pre_commit_hooks/get_github_actions_doc.py
@@ -16,11 +16,11 @@ def main(argv: Sequence[str] | None = None) -> int:
             with open(filename, encoding='UTF-8') as f:
                 actions = yaml.load(f)
                 readme=open('README.md', encoding='UTF-8').read()
-                pattern = re.compile(r"""<!--BEGIN DOC-->.*<!--END DOC>""",re.DOTALL)
+                pattern = re.compile(r"""<!--BEGIN DOC-->.*<!--END DOC>""",re.DOTALL, re.MULTILINE)
                 if pattern.match(readme):
-                    readme = pattern.sub("<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->", readme)            
+                    readme = pattern.sub("\n<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->\n", readme)            
                 else:
-                    readme = readme + "<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->"            
+                    readme = readme + "\n<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->\n"            
                 open("README.md", 'wb').write(readme.encode("utf-8"))
         except yaml.YAMLError as exc:
             print(exc)
