a175dbc1ea367c777f2d3d225cdeea1a82e8eb5d
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 14:33:25 -0300

    reste

--- pre_commit_hooks/get_github_actions_doc.py
+++ pre_commit_hooks/get_github_actions_doc.py
@@ -19,7 +19,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
             readme=open('README.md', encoding='UTF-8').read()
             pattern = re.compile(r"<!--BEGIN_DOC-->.*?<!--END_DOC-->", flags=re.DOTALL)
-            if re.match(r'(?P<text>.*)<!--END_DOC-->', readme):
+            if pattern.match(readme):
                 readme = pattern.sub("\n<!--BEGIN_DOC-->\nSecond Text\n<!--END_DOC-->\n", readme)
             else:
                 readme = readme + "\n<!--BEGIN_DOC-->\nFirst Text\n<!--END_DOC-->\n"
