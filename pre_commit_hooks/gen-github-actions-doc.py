03eb41a8a2444ef3d1aba8c51a5fb122a87cf5fe
Gerson Graciani <15052330+gracig@users.noreply.github.com>
2022-06-28 12:37:04 -0300

    Add the workflow file argument

--- pre_commit_hooks/gen-github-actions-doc.py
+++ pre_commit_hooks/gen-github-actions-doc.py
@@ -8,30 +8,28 @@ from pre_commit_hooks.check_yaml import yaml
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('workflow_file', nargs='*', help='Filenames to check.')
+    parser.add_argument('workflow_file', nargs='1', help='Filenames to check.')
     args = parser.parse_args(argv)
-
     retval = 0
-    for filename in args.filenames:
-        try:
-            with open(filename, encoding='UTF-8') as f:
-                actions = yaml.load(f)
-                readme=""
-                with open("README.md", 'r') as f:
-                    readme = f.read().decode('utf8')
-                pattern = re.compile(r"""<!--BEGIN DOC-->.*<!--END DOC>""",re.MULTILINE)
-                if pattern.match(readme):
-                    readme = pattern.sub("<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->", readme)            
-                else:
-                    readme = readme + "<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->"            
-                with open("README.md", 'w') as f:
-                    f.write(readme.encode("utf-8"))
+    try:
+        with open(args.workflow_file, encoding='UTF-8') as f:
+            actions = yaml.load(f)
+            readme=""
+            with open("README.md", 'r') as f:
+                readme = f.read().decode('utf8')
+            pattern = re.compile(r"""<!--BEGIN DOC-->.*<!--END DOC>""",re.MULTILINE)
+            if pattern.match(readme):
+                readme = pattern.sub("<!--BEGIN DOC-->\nSecond Text\n<!--END DOC-->", readme)            
+            else:
+                readme = readme + "<!--BEGIN DOC-->\nFirst Text\n<!--END DOC-->"            
+            with open("README.md", 'w') as f:
+                f.write(readme.encode("utf-8"))
 
-        except yaml.YAMLError as exc:
-            print(exc)
-            retval = 1
-        except:
-            retval = 1
+    except yaml.YAMLError as exc:
+        print(exc)
+        retval = 1
+    except:
+        retval = 1
 
     return retval
 
